2a2620de5e689eb56659f77e85126a2dd2ed2018
bogang.tian <bogang.tian@gmail.com>
2020-10-19 17:30:17 -0400

    Updates

---
+++ precache-manifest.9dca89a8c241ea2e4891dc5520d54c96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de0dc7b5a6ceeebacdad188c45565de",
+    "url": "/matching-game-react/index.html"
+  },
+  {
+    "revision": "a5a252da080328eebb0b",
+    "url": "/matching-game-react/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "14d010571d26ba6cc243",
+    "url": "/matching-game-react/static/css/main.8ab75914.chunk.css"
+  },
+  {
+    "revision": "a5a252da080328eebb0b",
+    "url": "/matching-game-react/static/js/2.4f44f54a.chunk.js"
+  },
+  {
+    "revision": "f406aedd96f61bd83ab19677f9afa536",
+    "url": "/matching-game-react/static/js/2.4f44f54a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d010571d26ba6cc243",
+    "url": "/matching-game-react/static/js/main.4bb93bc1.chunk.js"
+  },
+  {
+    "revision": "54fae727e8bc66628462",
+    "url": "/matching-game-react/static/js/runtime-main.13e62a40.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/matching-game-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
