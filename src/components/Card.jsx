2d94f5e5915acfa022fbc69ec13ecd62a8213be1
bogang.tian <bogang.tian@gmail.com>
2020-10-22 10:08:23 -0400

    removed useStyle

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,107 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { createUseStyles } from "react-jss";
 
 import { CardState } from '../store/game';
+import './Card.css';
 
-const useStyles = createUseStyles({
-    card: {
-        width: 50,
-        height: 50,
-        marginLeft: 10,
-        marginRight: 10,
-        marginTop: 10,
-        marginBottom: 10,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        outline: "none !important",
-        border: "0px solid",
-        borderRadius: 4
-    },
-    cardClosed: {
-        extend: "card",
-        backgroundColor: "white"
-    },
-    '@keyframes spin-to-green': {
-        from: {
-            color: "black",
-            backgroundColor: "white",
-            transform: "rotateY(180deg)"
-        },
-        to: {
-            color: "white",
-            backgroundColor: "green",
-            transform: "rotateY(0deg)"
-        }
-    },
-    cardGreen: {
-        extend: "card",
-        color: "white",
-        backgroundColor: "green",
-        animationName: "$spin-to-green",
-        animationDuration: "0.5s",
-        animationTimingFunction: "linear"
-    },
-    '@keyframes spin-to-red': {
-        from: {
-            transform: "rotateY(180deg)"
-        },
-        to: {
-            transform: "rotateY(0deg)"
-        }
-    },
-    cardRed: {
-        extend: "card",
-        color: "white",
-        backgroundColor: "red",
-        animationName: "$spin-to-red",
-        animationDuration: "0.5s",
-        animationTimingFunction: "linear"
-    },
-    '@keyframes spin-to-hidden': {
-        from: {
-            backgroundColor: "green"
-        },
-        to: {
-            backgroundColor: "transparent",
-            border: "0px white"
-        }
-    },
-    cardHidden: {
-        extend: "card",
-        animationName: "$spin-to-hidden",
-        animationDuration: "0.5s",
-        animationTimingFunction: "ease-out"
-    }
-});
+const ClosedCard = ({ point, onClickCarset }) => (
+    <button className="card card-closed" onClick={() => onClickCarset(point)}></button>
+);
 
-const ClosedCard = ({ point, onClickCarset }) => {
-    const style = useStyles();
-    return (
-        <button className={style.cardClosed} onClick={() => onClickCarset(point)}></button>
-    );
-};
+const OpenGreenCard = ({ point }) => (
+    <div className="card card-green">{point}</div>
+);
 
-const OpenGreenCard = ({ point }) => {
-    const style = useStyles();
-    return (
-        <div className={style.cardGreen}>{point}</div>
-    );
-};
+const OpenRedCard = ({ point }) => (
+    <div className="card card-red">{point}</div>
+);
 
-const OpenRedCard = ({ point }) => {
-    const style = useStyles();
-    return (
-        <div className={style.cardRed}>{point}</div>
-    );
-};
-
-const HiddenCard = () => {
-    const style = useStyles();
-    return (
-        <div className={style.cardHidden}></div>
-    );
-};
+const HiddenCard = () => (
+    <div className="card card-hidden"></div>
+);
 
 const Card = ({ card, onClickCarset }) => {
     const status = card.status;
